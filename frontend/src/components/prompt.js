a6dee9f1ebc0611c25b0c03038db356bb54bc77a
ahuang95 <andy0207huang@gmail.com>
2023-06-24 12:27:39 -0400

    frontend: add colour loaded ring

--- frontend/src/components/prompt.js
+++ frontend/src/components/prompt.js
@@ -3,22 +3,51 @@ import { imageFromTextPrompt } from "../api";
 
 export const TextPromptForm = () => {
   const [prompt, setPrompt] = useState("");
+  const [isLoading, setIsLoading] = useState(false)
 
+  // Text Prompt
   const handleInputChange = (event) => {
     setPrompt(event.target.value);
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    setIsLoading(true);
+
     const response = await imageFromTextPrompt(prompt);
     console.log(response);
+    setPost(response.result);
+
+    setIsLoading(false);
   };
+
+  // Image Prompt
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
+        {/* Text Prompt */}
         <input type="text" value={prompt} onChange={handleInputChange} />
+
+        {/* Image Upload Prompt */}
+
+        {/* Submit Prompt */}
         <button type="submit">Submit</button>
+
+        {/* Display submit data */}
+        <p>{isLoading ?
+
+          <ColorRing
+            visible={true}
+            height="180"
+            width="180"
+            ariaLabel="blocks-loading"
+            wrapperStyle={{}}
+            wrapperClass="blocks-wrapper"
+            colors={['#FFFF00', '#ED1C24', '#0476C0', '#333333', '#57DBF2']}
+          />
+
+          : post ? post : ""}</p>
       </form>
     </div>
   );
