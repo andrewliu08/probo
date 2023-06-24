cb9cd3206bbc7e0bef0091ca7ececa304713912e
ahuang95 <andy0207huang@gmail.com>
2023-06-24 18:29:17 -0400

    add IDKitWidget for login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { ConnectMetaMaskButton } from "./components/wallet";
 import { DisplayNFT } from "./components/displayNFT";
 import { TextPromptForm } from "./components/prompt";
+import { useIDKit, IDKitWidget } from '@worldcoin/idkit'
+
 import "./App.css";
 
 function App() {
@@ -8,8 +10,20 @@ function App() {
     <div className="App">
       <header className="App-header">
         <DisplayNFT />
+
         <ConnectMetaMaskButton />
+
+        <IDKitWidget
+          app_id="app_staging_c0520c3180cae840e95ed335c4447cbf" // obtain this from developer.worldcoin.org
+          action=""
+          enableTelemetry
+          onSuccess={result => console.log("success")}
+        >
+          {({ open }) => <button onClick={open}>Click me</button>}
+        </IDKitWidget>
+
         <TextPromptForm />
+
       </header>
     </div>
   );
